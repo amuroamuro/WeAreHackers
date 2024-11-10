d1ecfdcf70b1b08461a699f3fc6fd095e168393d
amuroamuro <mbhstar1206@gmail.com>
2024-11-10 12:48:19 +0900

    js파잃 생성

---
+++ main.js
@@ -0,0 +1,19 @@
+// 시스템바 불러오기
+const help = document.querySelector("#help");
+const store = document.querySelector("#store");
+const auto = document.querySelector("#store+button");
+const cash = document.querySelector("div label");
+// 아웃풋 불러오기
+const output = document.querySelector("div+label");
+//프롬프트
+const prompt = document.querySelector("input");
+//환경 설정
+let finding = false;
+let server = false;
+//기타 변수
+let commend = "";
+let Cash = 0;
+
+
+
+
